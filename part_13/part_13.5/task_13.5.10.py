fe805cc8c2c92ac3af658f45de389348c9c755f6
AsRomanista <dmitry.garkovitch@gmail.com>
2024-10-10 13:32:12 +0200

    new task 13.5.10

---
+++ part_13/part_13.5/task_13.5.10.py
@@ -0,0 +1,15 @@
+# Напишите функцию convert_to_python_case(text),
+# которая принимает в качестве аргумента строку в «верблюжьем регистре» и преобразует его в «змеиный регистр».
+
+def convert_to_python_case(text):
+    snake_text = ''
+    for i in range(len(text)):
+        if text[i] == text[i].upper() and i != 0 and not text[i].isdigit():
+            snake_text += ('_' + text[i])
+        else:
+            snake_text += text[i]
+    return snake_text.lower()
+
+txt = input()
+
+print(convert_to_python_case(txt))
